4ff094868e40fc82961f163dac895e40c9e90265
jammin2shirts <teuchertb@gmail.com>
2019-01-01 20:21:49 -0500

    initial commit

---
+++ TheRssed/src/main/java/com/jammin2shirts/TheRssed/TheRssedApplication.java
@@ -0,0 +1,14 @@
+package com.jammin2shirts.TheRssed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRssedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRssedApplication.class, args);
+	}
+
+}
+
---
+++ TheRssed/src/test/java/com/jammin2shirts/TheRssed/TheRssedApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jammin2shirts.TheRssed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheRssedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
