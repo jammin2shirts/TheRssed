a5779cba0664d0d765a00e3eebaeb235b5685c10
jammin2shirts <teuchertb@gmail.com>
2019-01-24 23:28:25 -0500

    Got feed to display successfully for static url, need to firgure out how to dynamically. Also needs to do the same for rss titles in the url area

--- TheRssed/src/main/java/com/jammin2shirts/TheRssed/Controllers/RssFeedController.java
+++ TheRssed/src/main/java/com/jammin2shirts/TheRssed/Controllers/RssFeedController.java
@@ -1,5 +1,8 @@
 package com.jammin2shirts.TheRssed.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,20 +17,20 @@ public class RssFeedController {
 	final static String joeRss="http://joeroganexp.joerogan.libsynpro.com/rss";
 	final static String mangaRss="https://readms.net/rss";
 	
-	@GetMapping("/rssFeeds")
-	public String retrieveFeed(@RequestParam(name="url", required=false, defaultValue=mangaRss) String url, Model model) {
+	@GetMapping("/")
+	public String retrieveFeed(@RequestParam(name="url", required=false, defaultValue=joeRss) String url, Model model) {
 		RSSFeedParser parser = new RSSFeedParser(url);
 		Feed feed = parser.readFeed();
+		
 		model.addAttribute("feed", feed);
 		
+		List<String> urls = new ArrayList<>();
+		urls.add(joeRss);
+		urls.add(mangaRss);
 		
-		
-		return "DisplayRssFeeds";
+		model.addAttribute("urls", urls);
+		return "index";
 		
 	}
 	
-	@GetMapping("/tester")
-	public String tester() {
-		return "tester";
-	}
 }
